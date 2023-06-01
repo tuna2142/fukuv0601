7fa34c2ee17f4e345686309c8e62182ea87ba82b
hikaru1224 <130636591+tuna2142@users.noreply.github.com>
2023-06-01 15:41:24 +0900

    演習　8-4

--- fukuv0601/Form1.Designer.cs
+++ fukuv0601/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(365, 216);
+            label1.Location = new Point(354, 211);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 0;
--- fukuv0601/Form1.cs
+++ fukuv0601/Form1.cs
@@ -2,7 +2,8 @@ namespace fukuv0601
 {
     public partial class Form1 : Form
     {
-        int vx = 10;
+        int vx = -10;
+        int vy = -10;
 
         public Form1()
         {
@@ -22,6 +23,16 @@ namespace fukuv0601
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left <= 0)
+                vx = 10;
+            if (label1.Left >= 760)
+                vx = -10;
+            if (label1.Top <= 0)
+                vy = 10;
+            if (label1.Top >= 440)
+                vy = -10;
         }
 
         private void label1_Click(object sender, EventArgs e)
